008e5cdc9d591a3feed5899c282801e4d1bd88f6
João Vitor <joaovytor0@gmail.com>
2019-03-22 21:24:58 -0300

    Adiciona algoritmo de busca sequencial
    Co-authored-by: João Pedro Soares <jpcirqueira81@gmail.com>

---
+++ busca_sequencial.py
@@ -0,0 +1,51 @@
+import random
+import time
+
+def busca_sequencial(elementos_lista, elemento):
+    i = 0
+    try:
+        while i <= len(elementos_lista):
+            if elementos_lista[i] == elemento:
+                break
+            i += 1
+        return i
+    except IndexError:
+        print('Elemento nao achado')
+
+
+def insere_randomico(lista, qtd_insercao):
+    for i in range(qtd_insercao):
+        lista.append(random.randint(1,10000))
+    random.shuffle(lista)
+    return lista
+
+def remove_randomico(lista, qtd_remocao):
+    for i in range(qtd_remocao):
+        lista.pop(random.randrange(len(lista)))    
+    return lista
+
+def main():
+    lista = random.sample(range(0,10 ** 4), 10 ** 4)
+    for i in range(0,20):
+        operation = random.randint(1,10)        
+        if 1 <= operation <= 8:
+            print('#'*100)
+            print('Realiza busca sequencial')
+            elem = random.randint(1, 11 ** 4)
+            print('Tamanho lista:',len(lista))
+            print('Elemento procurado:',elem)
+            start = time.time()
+            busca_sequencial(lista, elem)
+            end = time.time()
+            elapsed_time = end - start
+            print('Tempo para realizar busca sequencial:', elapsed_time)
+        elif operation == 9:
+            print('#'*100)
+            print('Insere elementos da lista')
+            lista = insere_randomico(lista, 10 ** 3)
+        elif operation == 10:
+            print('#'*100)
+            print('Removendo elementos da lista')
+            lista = remove_randomico(lista, 9 ** 3)
+
+main()
